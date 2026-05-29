c3462d246daafe1fcdcb2f6cd300197867a4de98
Kassandra <kam.terbeek@gmail.com>
2026-05-28 22:05:47 -0500

    Update __init__.py

--- functorch/_src/aot_autograd/__init__.py
+++ functorch/_src/aot_autograd/__init__.py
@@ -1,4 +1,4 @@
-# This file has moved to under torch/_functorch. It is not public API.
+# This file moved to under torch/_functorch. It is not public API.
 # If you are not a PyTorch developer and you are relying on the following
 # imports, please file an issue.
 from torch._functorch.aot_autograd import (
